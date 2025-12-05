3b2d5ce9801dda40c2395787d5c6ab55bce55b12
joaopedreira-planejar <joao.pedreira@planejarjf.com.br>
2025-12-05 11:45:23 -0300

    remove arquivos de exemplo

--- modules/Produtos/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('produtos')->name('produtos.')->group(function () {
-    Route::get('/doly', function () { return 'dale12345';})->name('index');
-});
--- modules/Users/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Users\Http\Controllers\GetUserController;
-
-Route::prefix('users')->name('users.')->group(function () {
-    Route::get('/doly', GetUserController::class)->name('index');
-});
